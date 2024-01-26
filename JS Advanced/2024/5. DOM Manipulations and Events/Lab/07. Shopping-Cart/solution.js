735d8a0655c78a3a95f33d69922aa97e05ff89b5
Anton <abbelkov@gmail.com>
2024-01-26 22:13:12 +0200

    up

--- JS Advanced/2024/5. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
+++ JS Advanced/2024/5. DOM Manipulations and Events/Lab/07. Shopping-Cart/solution.js
@@ -1,19 +1,32 @@
 function solve() {
-      let result = {};
-      let addBtns = document.querySelectorAll('.add-product');
+   let result = {};
+   let addBtns = document.querySelectorAll('.add-product');
+   for (let btn of addBtns) {
+      btn.addEventListener('click', addBtnClick);
+   }
+
+   let resultBtn = document.querySelector('.checkout');
+   resultBtn.addEventListener('click', onCheckout);
+
+   function addBtnClick(event) {
+      let currentBtn = event.target;
+      let productName = currentBtn.parentElement.parentElement.querySelector('.product-title').textContent;
+      let productPrice = currentBtn.parentElement.parentElement.querySelector('.product-line-price').textContent;
+      if (!result.hasOwnProperty(productName)) {
+         result[productName] = 0;
+      }
+
+      result[productName] += Number(productPrice);
+      document.querySelector('textarea').textContent += `Added ${productName} for ${productPrice} to the cart.\n`
+   }
+
+   function onCheckout(e) {
+      let listOfProducts = Object.keys(result);
+      let totalPrice = Object.values(result).reduce((a, b) => a + b, 0);
+      document.querySelector('textarea').textContent += `You bought ${listOfProducts.join(', ')} for ${totalPrice.toFixed(2)}.`
+      resultBtn.disabled = true;
       for (let btn of addBtns) {
-         btn.addEventListener('click', addBtnClick);
+         btn.disabled = true;
       }
-
-      function addBtnClick(event) {
-         let currentBtn = event.target;
-         let productName = currentBtn.parentElement.parentElement.querySelector('.product-title').textContent;
-         let productPrice = currentBtn.parentElement.parentElement.querySelector('.product-line-price').textContent;
-         if (!result.hasOwnProperty(productName)) {
-            result[productName] = Number(productPrice);
-         }
-
-         result[productName] += Number(productPrice);
-         document.querySelector('textarea').textContent += `Added ${productName} for ${productPrice} to the cart.\n`
-      }
+   }
 }
