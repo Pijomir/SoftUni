e3f4af2df1aa889fef14a981be83e9bf94b8333a
Anton <abbelkov@gmail.com>
2024-12-28 12:15:40 +0200

    delete

--- JS Advanced/2024/JS Applications/4. Single Page Applications/Exercises/01.Forum/src/app.js
+++
@@ -1,8 +0,0 @@
-import { loadHome } from "./home.js";
- 
-loadHome();
-const homeBtn = document.querySelector("nav ul li a");
-homeBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    loadHome();
-});
